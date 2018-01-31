d647eb6a264c8933d7d37250a86cf6d5d8c4f7e6
ghodgaonkaraditya <ghodgaonkaraditya@gmail.com>
2018-01-31 18:39:13 +0530

    Assignments of class 10

---
+++ ReadMulti.java
@@ -0,0 +1,38 @@
+
+package Files;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ReadMulti {
+    
+    public static void main(String args[]) throws IOException{
+        
+        File fobject=new File("C:\\Users\\Aditya\\Desktop\\pathMulti.txt");
+        
+        try {
+            FileReader in_stream=new FileReader(fobject);
+            int c;
+
+            while((c=in_stream.read())!=-1){
+               System.out.print(""+((char)c));  
+            }
+           
+            
+        } catch (FileNotFoundException ex) {
+          System.out.print("File not Found!!");  
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
